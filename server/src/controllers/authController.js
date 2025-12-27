d42fb858b760c8b9374d72f4ab485491241b44ae
Odurukeerthan <keerthanoduru39@gmail.com>
2025-12-27 18:13:13 +0530

    feat: implement user registration with password hashing and JWT

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,25 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ message: "User exists" });
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.status(201).json({ token });
+};
