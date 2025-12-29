366901bf9f36b00fb6365f6b532eaa695019b45f
Odurukeerthan <keerthanoduru39@gmail.com>
2025-12-29 21:58:15 +0530

    feat: add user login API with JWT authentication

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -23,3 +23,22 @@ export const registerUser = async (req, res) => {
 
   res.status(201).json({ token });
 };
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.json({ token });
+};
