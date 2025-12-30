998b106b2d388024883b8878031693377851fb3f
Odurukeerthan <keerthanoduru39@gmail.com>
2025-12-30 22:46:41 +0530

    feat : add curated skill dictionary for resume analysis

---
+++ ai-service/skills.py
@@ -0,0 +1,20 @@
+SKILLS = {
+    "programming_languages": [
+        "python", "c", "c++", "java", "javascript"
+    ],
+    "frontend": [
+        "html", "css", "bootstrap", "react", "react js","tailwind css"
+    ],
+    "backend": [
+        "node.js", "express", "mongodb", "mysql", "php"
+    ],
+    "ai_ml": [
+        "machine learning", "artificial intelligence", "nlp"
+    ],
+    "tools": [
+        "git", "github", "vs code", "spyder"
+    ],
+    "os": [
+        "windows", "ubuntu", "linux"
+    ]
+}
