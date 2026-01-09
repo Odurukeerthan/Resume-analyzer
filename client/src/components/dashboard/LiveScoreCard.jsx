27686d0fecc1cd9266ac4c935e60ea069ee94614
Odurukeerthan <keerthanoduru39@gmail.com>
2026-01-09 23:30:36 +0530

    Formating and adding styles

--- client/src/components/dashboard/LiveScoreCard.jsx
+++ client/src/components/dashboard/LiveScoreCard.jsx
@@ -1,17 +1,24 @@
 import ScoreRing from "../ScoreRing";
-
+import LiveScore from "../../assets/live-score.svg";
 export default function LiveScoreCard() {
   return (
-    <div className="bg-cardDark rounded-xl border-r-4 border-neonPurple p-6 flex flex-col items-center justify-center shadow-[0_0_6px_rgba(168,85,247,0.35),inset_-4px_0_8px_rgba(168,85,247,0.25)]">
-      <h3 className="mb-4 flex items-center gap-2">
-        <span className="text-neonPurple">●</span> Live Score
-      </h3>
+    <div className="bg-cardDark rounded-xl border-r-4 border-neonPurple p-6 flex flex-col shadow-[0_0_10px_rgba(192,132,252,0.2),inset_-4px_0_5px_1px_rgba(192,132,252,0.1)] h-full">
+  
+      <div className="flex items-center gap-2 w-full mb-2">
+        <img src={LiveScore} alt="Live Score" className="w-6 h-6" />
+        <h3 className="font-sans text-[18px] font-semibold">
+          Live Score
+        </h3>
+      </div>
 
+    <div className="flex flex-col items-center justify-center grow w-full py-4">
       <ScoreRing score={89} />
 
-      <p className="mt-4 text-sm text-gray-400">
+      <p className="mt-4 text-sm text-gray-400 font-mono">
         EXCELLENT
       </p>
     </div>
+    
+  </div>
   );
 }
